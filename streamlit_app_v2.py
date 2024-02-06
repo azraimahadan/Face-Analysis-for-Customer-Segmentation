268117a580719b4de8ecd3bd5dafc6dc32de57a6
LordAx <azrai.mok@gmail.com>
2024-02-06 18:19:44 +0800

    dfdf

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -133,8 +133,8 @@ def main():
         key="object-detection",
         mode=WebRtcMode.SENDRECV,
         rtc_configuration={
-            "iceServers": get_ice_servers(),
-            "iceTransportPolicy": "relay",
+            "iceServers": [],
+            "iceTransportPolicy": "all",
         },
     video_frame_callback=video_frame_callback,
     media_stream_constraints={"video": True, "audio": False},
